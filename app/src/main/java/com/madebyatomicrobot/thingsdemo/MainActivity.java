1638e3644813406b8b7862110ad2f33e7c2eacc1
Patrick Hammond <patrick@madebyatomicrobot.com>
2017-04-12 22:35:46 -0400

    Adding Android Things to the project configuration.

--- app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.java
+++ app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.madebyatomicrobot.thingsdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.things.pio.PeripheralManagerService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Cleanup goes here!
+        super.onDestroy();
     }
 }
