73bb8d91a2cd54c30668877d0603256cbbd389e1
Patrick Hammond <patrick@madebyatomicrobot.com>
2017-04-12 22:52:56 -0400

    Toggling a LED.

--- app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.java
+++ app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.java
@@ -2,22 +2,67 @@ package com.madebyatomicrobot.thingsdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ToggleButton;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+
+    private PeripheralManagerService service;
+    private ToggleButton ledToggleView;
+    private Gpio pin21;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        service = new PeripheralManagerService();
 
-        PeripheralManagerService service = new PeripheralManagerService();
+        setupDemo1();
+    }
+
+    private void setupDemo1() {
+        ledToggleView = (ToggleButton) findViewById(R.id.ledToggle);
+
+        try {
+            pin21 = service.openGpio("BCM17");
+            pin21.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException ex) {
+            Log.e(TAG, "Error during onCreate!", ex);
+        }
+
+        try {
+            ledToggleView.setChecked(pin21.getValue());
+        } catch (IOException ex) {
+            Log.e(TAG, "Error during setChecked!", ex);
+        }
+
+        ledToggleView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    pin21.setValue(isChecked);
+                } catch (IOException ex) {
+                    Log.e(TAG, "Error during onCheckedChanged!", ex);
+                }
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
-        // Cleanup goes here!
+        try {
+            pin21.close();
+        } catch (IOException ex) {
+            Log.e(TAG, "Error during onDestroy!", ex);
+        }
         super.onDestroy();
     }
 }
