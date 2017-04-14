83ac5575c45d006eb9a189b3fd78045a67a3a488
Patrick Hammond <patrick@madebyatomicrobot.com>
2017-04-13 23:04:59 -0400

    Moving second demo to Kotlin.

--- app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.kt
+++ app/src/main/java/com/madebyatomicrobot/thingsdemo/MainActivity.kt
@@ -2,17 +2,23 @@ package com.madebyatomicrobot.thingsdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.ToggleButton
 import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManagerService
 
-class MainActivity : AppCompatActivity() {
 
+
+class MainActivity : AppCompatActivity() {
     private lateinit var service: PeripheralManagerService
 
     private lateinit var ledToggleView: ToggleButton
 
     private lateinit var pin17: Gpio
+
+    private lateinit var pin22: Gpio
+    private lateinit var pin22Callback: GpioCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         service = PeripheralManagerService()
 
         setupDemo1()
+        setupDemo2()
     }
 
     private fun setupDemo1() {
@@ -33,8 +40,30 @@ class MainActivity : AppCompatActivity() {
         ledToggleView.setOnCheckedChangeListener { _, isChecked -> pin17.value = isChecked }
     }
 
+    private fun setupDemo2() {
+        pin22 = service.openGpio("BCM22")
+        pin22.setDirection(Gpio.DIRECTION_IN)
+        pin22.setActiveType(Gpio.ACTIVE_HIGH)
+        pin22.setEdgeTriggerType(Gpio.EDGE_FALLING)
+
+        pin22Callback = object : GpioCallback() {
+            override fun onGpioEdge(gpio: Gpio?): Boolean {
+                ledToggleView.isChecked = !ledToggleView.isChecked
+                return true
+            }
+
+            override fun onGpioError(gpio: Gpio?, error: Int) {
+                Log.e("Things", "Error during pin22 gpio callback: " + error)
+            }
+        }
+        pin22.registerGpioCallback(pin22Callback)
+    }
+
     override fun onDestroy() {
         pin17.close()
+
+        pin22.unregisterGpioCallback(pin22Callback)
+        pin22.close()
 
         super.onDestroy()
     }
